46fc1310227acb17c9fb021c53adfed22eecc070
Ani03G <anigharagyozyan03@gmail.com>
2026-03-04 23:31:28 +0400

    fix base href

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,18 +32,18 @@ const RESOURCES = {"assets/AssetManifest.bin": "bf46ca385793fd450b48008d28d320e6
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "f340965480a7a6a0269ddd739d005b1e",
+"flutter_bootstrap.js": "96fa0e661b440d5ea792400ca1471a63",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "083f7be9611ec39a13da61b130ac803c",
-"/": "083f7be9611ec39a13da61b130ac803c",
+"index.html": "bddaf8a4c5716e65208b68fedbb410f3",
+"/": "bddaf8a4c5716e65208b68fedbb410f3",
 "main.dart.js": "0e6ab253b36c4d61816f526e3663a471",
 "manifest.json": "70f22d10eab8f2367a91d0bca3373ddc",
 "version.json": "291959236343470b41af98b64c3540ee",
 "web.zip.zip": "7faad8388f6d614155ddd6fb30beb599",
-"_redirects": "d38a2b58df330c85e0029eecf71d7c26"};
+"_redirects": "ef952d360ec42c652095dfeb72a350a6"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
